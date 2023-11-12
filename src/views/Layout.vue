bb979f6d9260ce4c7c29523ef025d5a3e3c33511
zwwin <izhaowantg@126.com>
2023-11-12 10:38:09 +0800

    modified content layout

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -144,7 +144,7 @@ const goBOke = () => {
         <el-main class="main_content">
           <router-view> </router-view>
         </el-main>
-        <el-footer>大事件 ©2023 Created by 黑马程序员</el-footer>
+        <el-footer>you can concat me via email izhaowang@126.com</el-footer>
       </el-main>
     </el-container>
   </div>
